babc1a31627cf9e6373ca576236a14209a46d601
DaniilUT <utemov-daniil@mail.ru>
2014-05-04 19:23:52 +0700

    Create min-max.rb

---
+++ utemov/min-max.rb
@@ -0,0 +1,14 @@
+require 'set'
+
+def rand_n(n, max)
+    randoms = Set.new
+    loop do
+        randoms << rand(max)
+        return randoms.to_a if randoms.size >= n
+    end
+end
+
+numbers = rand_n(2, 50)
+p numbers
+p numbers.max
+p numbers.min
