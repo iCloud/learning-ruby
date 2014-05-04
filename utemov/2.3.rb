192e4e0f5e23fb37166dbfbfab95f80a0242d4bf
DaniilUT <utemov-daniil@mail.ru>
2014-05-04 20:05:37 +0700

    Create 2.3.rb

---
+++ utemov/2.3.rb
@@ -0,0 +1,22 @@
+require 'set'
+
+def rand_n(n, max)
+    randoms = Set.new
+    loop do
+        randoms << rand(max*2)-max
+        return randoms.to_a if randoms.size >= n
+    end
+end
+
+
+numbers = rand_n(3, 50)
+p numbers
+numbers.map! do |i|
+	if i > 0 
+		i ** (2)
+	else
+		i
+	end
+end
+		
+p numbers
