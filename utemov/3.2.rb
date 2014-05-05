b31e761a2e015bfc8a2912945239a9a6887156fe
DaniilUT <utemov-daniil@mail.ru>
2014-05-05 11:25:34 +0700

    Update 3.2.rb

--- utemov/3.2.rb
+++ utemov/3.2.rb
@@ -1,14 +1,6 @@
 #Дано натуральное число n (n>99). Определить число сотен в нем. 
 require 'set'
 
-def rand_n(n, max)
-    randoms = Set.new
-    loop do
-        randoms << rand(max*2)-max
-        return randoms.to_a if randoms.size >= n
-    end
-end
-
 a = rand(1000)+100
 p a
 p a/100
